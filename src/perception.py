204cd4cf2fa55ac497dd334abe1ca2a77f7f903f
Sifan Jiang <jasper.sifan@gmail.com>
2019-09-10 17:15:03 +0200

    update

--- src/perception.py
+++ src/perception.py
@@ -1,61 +1,36 @@
 '''
-Change the fields below for your code to make it more presentable:
+@author:        Sifan Jiang
+email:          sifanj@kth.se
+Date:           2019/9/10   
 
-@author :       Ajinkya Khoche
-email:          khoche@kth.se
-Date:           2018/09/03   
-Description:    This program
-                - Reads a video from 'test_videos' folder and reads every frame 
-                till end of video. It stores every frame in variable of same name.
-                - Your algorithm should process 'frame' variable (or frame_cloned. 
-                its good to clone the frame to preserve original data)
-                - The result of your algorithm should be lists of 'bounding_boxes'
-                and 'labels'. 
-                - The helper code takes 'bounding_boxes' to draw rectangles on the
-                positions where you found the cones. It uses corresponding 'labels'
-                to name which type of cone was found within 'bounding_boxes'.  
-
-                Color Convention that we follow:
-                ---------------- 
-                    0-  YELLOW
-                    1-  BLUE
-                    2-  ORANGE
-                    3-  WHITE
-                    4-  BLACK
-
-                This basically means that if labels[i] = 0, then you can set the i_th
-                bounding_box as 'yellow cone'    
+Color Convention that we follow:
+---------------- 
+    0-  YELLOW
+    1-  BLUE
+    2-  ORANGE
+    3-  WHITE
+    4-  BLACK
 '''
+
 import numpy as np
 from cv2 import cv2
 
 def main():
-    # Read video from disk and count frames
-    cap = cv2.VideoCapture('./Videos/Formula Student Spain 2015 Endurance- DHBW Engineering with the eSleek15.mp4')
+    cap = cv2.VideoCapture('./Videos/Video_1.mp4')
     frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     count = 0
-    
-    '''
-    NOTE: For this example, bounding_box is manually defined.
-    For your assignment you need to comment the definitions of
-    'bounding_boxes' and 'labels' below and use bounding_box 
-    obtained from your algorithm. Its the structure which is 
-    provided
-    '''
+
     bounding_box = []
-    bounding_box.append(np.array((100,100,150,200)))
-    bounding_box.append(np.array((200,200,300,400)))
-    bounding_box.append(np.array((500,500,600,700)))
-    
-    labels = [0,2,1]
+    labels = []
+    # bounding_box.append(np.array((100,100,200,100)))
+    # labels.append(0)
 
     # Read every frame till the end of video
     while count < frameCount:
         ret, frame = cap.read()
         if ret == True:
-            count = count + 1
-
+            count += 1
             frame_cloned = np.copy(frame)
             '''
             #
@@ -90,22 +65,29 @@ def main():
                 '''
                 xmin = box[0]
                 ymin = box[1]
-                xmax = box[2]   # w = box[2]
-                ymax = box[3]   # h = box[3]
+                # xmax = box[2]
+                w = box[2]
+                # ymax = box[3]
+                h = box[3]
 
                 if labels[i] == 0:
-                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0, 255, 255), 5)     #cv2.rectangle(frame_cloned ,(xmin,ymin), (xmin + w,ymin + h), (0,255,0), 5)
-                    cv2.putText(frame_cloned, 'yellow cone', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+                    # cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0, 255, 255), 5)
+                    cv2.rectangle(frame_cloned, (xmin,ymin), (xmin+w,ymin+h), (0,255,255), 5)
+                    cv2.putText(frame_cloned, 'Y', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
                 if labels[i] == 1:
-                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (255, 0, 0), 5)     #cv2.rectangle(frame_cloned ,(xmin,ymin), (xmin + w,ymin + h), (0,255,0), 5)
-                    cv2.putText(frame_cloned, 'blue cone', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+                    # cv2.rectangle(frame_cloned ,(xmin, ymin), (xmax,ymax), (255, 0, 0), 5)
+                    cv2.rectangle(frame_cloned, (xmin,ymin), (xmin+w,ymin+h), (255,0,0), 5)
+                    cv2.putText(frame_cloned, 'B', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
                 if labels[i] == 2:
-                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0,165,255), 5)     #cv2.rectangle(frame_cloned ,(xmin,ymin), (xmin + w,ymin + h), (0,255,0), 5)
-                    cv2.putText(frame_cloned, 'orange cone', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+                    # cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0,165,255), 5)
+                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmin+w,ymin+h), (0,165,255), 5)
+                    cv2.putText(frame_cloned, 'O', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
             cv2.imshow('Original frame', frame)
             cv2.waitKey(10)
             cv2.imshow('Result of cone detection', frame_cloned)
             cv2.waitKey(10)
+
+
 if __name__ == '__main__':
     main()
