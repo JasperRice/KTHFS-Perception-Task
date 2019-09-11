4e02c5cf68d5bb85c793bf3f978d52b5f487960a
Sifan Jiang <sifanj@kth.se>
2019-09-11 08:27:24 +0200

    Add HSV.png

--- src/perception.py
+++ src/perception.py
@@ -22,15 +22,15 @@ def main():
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     count = 0
 
-    lowerY = np.array([0, 0, 0])
-    upperY = np.array([0, 255, 255])
+    lower__hsv_yellow = np.array([0, 0, 0])
+    upper_hsv_yellow = np.array([0, 255, 255])
 
-    lowerB = np.array([120, 0, 0])
-    upperB = np.array([255, 0, 0])
+    lower_hsv_blue = np.array([120, 0, 0])
+    upper_hsv_blue = np.array([255, 0, 0])
 
-    lowerO = np.array([0, 135, 135])
-    upperO = np.array([15, 255, 255])
-    
+    lower_hsv_orange = np.array([0, 135, 135])
+    upper_hsv_orange = np.array([15, 255, 255])
+
     while count < frameCount:
         ret, frame = cap.read()
         bounding_box = []
@@ -41,36 +41,36 @@ def main():
             frame_cloned = np.copy(frame)
 
             ##### Test
-            frame_hsv = cv2.cvtColor(frame_cloned, cv2.COLOR_BGR2HSV)
+            frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             frame_yellow = cv2.inRange(frame_hsv, lowerY, upperY)
             frame_blue = cv2.inRange(frame_hsv, lowerB, upperB)
             frame_orange = cv2.inRange(frame_hsv, lowerO, upperO)
             #####
 
             # Process cloned frame
-            bounding_box.append(np.array((100,100,200,100)))
-            labels.append(1)
+            # bounding_box.append(np.array((100,100,200,100)))
+            # labels.append(1)
 
             # Plot the bounding boxes
             for box, i in zip(bounding_box, range(len(bounding_box))):
                 xmin = box[0]
                 ymin = box[1]
-                # xmax = box[2]
-                # ymax = box[3]
-                w = box[2]
-                h = box[3]
+                xmax = box[2]
+                ymax = box[3]
+                # w = box[2]
+                # h = box[3]
 
                 if labels[i] == 0:
-                    # cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0, 255, 255), 5)
-                    cv2.rectangle(frame_cloned, (xmin,ymin), (xmin+w,ymin+h), (0,255,255), 5)
+                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0,255,255), 5)
+                    # cv2.rectangle(frame_cloned, (xmin,ymin), (xmin+w,ymin+h), (0,255,255), 5)
                     cv2.putText(frame_cloned, 'Y', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
                 if labels[i] == 1:
-                    # cv2.rectangle(frame_cloned ,(xmin, ymin), (xmax,ymax), (255, 0, 0), 5)
-                    cv2.rectangle(frame_cloned, (xmin,ymin), (xmin+w,ymin+h), (255,0,0), 5)
+                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (255,0,0), 5)
+                    # cv2.rectangle(frame_cloned, (xmin,ymin), (xmin+w,ymin+h), (255,0,0), 5)
                     cv2.putText(frame_cloned, 'B', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
                 if labels[i] == 2:
-                    # cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0,165,255), 5)
-                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmin+w,ymin+h), (0,165,255), 5)
+                    cv2.rectangle(frame_cloned ,(xmin,ymin), (xmax,ymax), (0,165,255), 5)
+                    # cv2.rectangle(frame_cloned ,(xmin,ymin), (xmin+w,ymin+h), (0,165,255), 5)
                     cv2.putText(frame_cloned, 'O', (int(xmin),int(ymin)-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
 
             cv2.imshow('Original frame', frame)
@@ -79,14 +79,14 @@ def main():
             cv2.waitKey(10)
 
             if TESTMODE:
-                cv2.imshow('HSV frame', frame_hsv)
-                cv2.waitKey(10)
+                # cv2.imshow('HSV frame', frame_hsv)
+                # cv2.waitKey(10)
                 cv2.imshow('Yellow frame', frame_yellow)
                 cv2.waitKey(10)
-                cv2.imshow('Blue frame', frame_blue)
-                cv2.waitKey(10)
-                cv2.imshow('Orange frame', frame_orange)
-                cv2.waitKey(10)
+                # cv2.imshow('Blue frame', frame_blue)
+                # cv2.waitKey(10)
+                # cv2.imshow('Orange frame', frame_orange)
+                # cv2.waitKey(10)
 
 
 if __name__ == '__main__':
